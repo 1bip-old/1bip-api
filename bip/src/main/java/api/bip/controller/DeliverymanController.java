f2b290821b66f441b67e10f28dbfa352af37a6b8
PC-GAMER\bruno <bruno.oliveira@itsolved.com.br>
2024-10-15 13:17:44 -0300

    alteracoes nas rotas e metodos no padrao

--- bip/src/main/java/api/bip/controller/DeliverymanController.java
+++ bip/src/main/java/api/bip/controller/DeliverymanController.java
@@ -5,9 +5,7 @@ import api.bip.persistence.service.DeliverymanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/deliveryman")
